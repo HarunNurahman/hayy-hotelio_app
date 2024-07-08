ad1de96d79726860e3965fdbf8a81f666ce9ad34
Harun Nurahman <harun.nurrahman@hotmail.com>
2024-07-08 11:45:37 +0700

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hayy_hotelio_app/bloc/auth/auth_bloc.dart';
-import 'package:hayy_hotelio_app/bloc/dashboard/dashboard_bloc.dart';
-import 'package:hayy_hotelio_app/bloc/hotel/hotel_bloc.dart';
 import 'package:hayy_hotelio_app/firebase_options.dart';
-import 'package:hayy_hotelio_app/pages/checkout-success_page.dart';
-import 'package:hayy_hotelio_app/pages/checkout_page.dart';
-import 'package:hayy_hotelio_app/pages/dashboard_page.dart';
-import 'package:hayy_hotelio_app/pages/onboarding_page.dart';
-import 'package:hayy_hotelio_app/pages/sign-in_page.dart';
-import 'package:hayy_hotelio_app/pages/sign-up_page.dart';
-import 'package:hayy_hotelio_app/services/session_service.dart';
+import 'package:hayy_hotelio_app/pages/splash_page.dart';
 import 'package:hayy_hotelio_app/shared/styles.dart';
-import 'package:intl/date_symbol_data_local.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-  initializeDateFormatting();
 
   runApp(const MyApp());
 }
@@ -31,47 +18,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => DashboardBloc()),
-        BlocProvider(create: (context) => AuthBloc()),
-        BlocProvider(create: (context) => HotelBloc()..add(GetListHotel())),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          scaffoldBackgroundColor: lightGrayColor,
-          appBarTheme: AppBarTheme(
-            backgroundColor: lightGrayColor,
-            elevation: 0,
-            centerTitle: true,
-            iconTheme: IconThemeData(color: blackColor),
-            titleTextStyle: blackTextStyle.copyWith(
-              fontSize: 18,
-              fontWeight: semiBold,
-            ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: lightGrayColor,
+        appBarTheme: AppBarTheme(
+          backgroundColor: lightGrayColor,
+          elevation: 0,
+          centerTitle: true,
+          iconTheme: IconThemeData(color: blackColor),
+          titleTextStyle: blackTextStyle.copyWith(
+            fontSize: 18,
+            fontWeight: semiBold,
           ),
         ),
-        routes: {
-          '/': (context) {
-            return FutureBuilder(
-              future: SessionService.getUser(),
-              builder: (context, snapshot) {
-                if (snapshot.data == null || snapshot.data!.id == null) {
-                  return const OnboardingPage();
-                } else {
-                  return const DashboardPage();
-                }
-              },
-            );
-          },
-          '/sign-in': (context) => const SignInPage(),
-          '/sign-up': (context) => const SignUpPage(),
-          '/dashboard': (context) => const DashboardPage(),
-          '/checkout': (context) => const CheckoutPage(),
-          '/checkout-success': (context) => const CheckoutSuccessPage(),
-        },
       ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashPage(),
+      },
     );
   }
 }
---
+++ lib/shared/app_format.dart
@@ -0,0 +1,36 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class AppFormat {
+  // Format tanggal bulan tahun
+  static String date(String stringDate) {
+    DateTime dateTime = DateTime.parse(stringDate);
+    return DateFormat('d MMM yyyy', 'en_US').format(dateTime); // 29 Feb 2020
+  }
+
+  // Format tanggal bulan
+  static String dateMonth(String stringDate) {
+    DateTime dateTime = DateTime.parse(stringDate);
+    return DateFormat('d MMM', 'en_US').format(dateTime); // 29 Feb
+  }
+
+  // Format currency
+  static String currency(num number) {
+    return NumberFormat.currency(
+      locale: 'en-US',
+      symbol: '\$',
+      decimalDigits: 0,
+    ).format(number);
+  }
+
+  // Custom snackbar
+  void showCustomSnackBar(BuildContext context, String message) {
+    Flushbar(
+      message: message,
+      flushbarPosition: FlushbarPosition.TOP,
+      backgroundColor: Colors.redAccent,
+      duration: const Duration(seconds: 3),
+    ).show(context);
+  }
+}
